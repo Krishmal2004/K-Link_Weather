1b4a3716b2ec18a51c14591c99476e7f22848e63
krishmal2004 <krishmaldinidu5466@gmail.com>
2026-01-17 16:55:58 +0530

    update the home page and create the weather page UI design

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wheather_application/main.dart';
 import 'package:wheather_application/home_page.dart';
 
@@ -7,6 +8,76 @@ class WeatherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        body: Stack(
+          children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/weather_page.jpg',
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height,
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'WEATHER IN\nNEW YORK',
+                        style: TextStyle(
+                          color: Colors.white24.withOpacity(0.8),
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Icon(
+                        Icons.settings_suggest_rounded,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.white24.withOpacity(0.5),
+                          thickness: 0.4,
+                          endIndent: 5,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'RAINY DAY',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      ),
+    );
   }
 }
